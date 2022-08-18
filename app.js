09b0ccb36c594b7374a64a29c9082a37e2b93140
Mohammad Zolghadr <empo1998124@gmail.com>
2022-08-18 14:16:44 +0430

    All Project Files

---
+++ app.js
@@ -0,0 +1,20 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-150%';
+}
+
+// const testImg = document.querySelector(".testImage");
+// testImg.style.backgroundImage = "url('./images/web.jpg')";
